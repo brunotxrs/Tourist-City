912f2cd9a712ba91960f51495ec7603e4afded97
brunotxrs <elielsonbrunno@gmail.com>
2024-12-06 16:29:41 -0400

    feat: creating navigation interaction on extra pages

---
+++ src/js/actions-pages.js
@@ -0,0 +1,17 @@
+var menuIcon = document.getElementById('menu-icon'); // Seleciona o ícone do menu
+var menu = document.querySelector('.menu'); // Seleciona o menu
+
+const imgClose = '../assets/icons/close-outline.svg';
+const imgOpen = '../assets/icons/reorder-four-outline.svg';
+
+
+menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('ativa'); // Alterna a classe 'ativa' no menu
+
+    // Verifica o estado e muda a imagem do ícone
+    if (menu.classList.contains('ativa')) {
+        menuIcon.src = imgClose; // Muda para o ícone "X"
+    } else {
+        menuIcon.src = imgOpen; // Muda de volta para o ícone original
+    }
+});
